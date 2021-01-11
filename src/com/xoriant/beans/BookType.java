d99eb3942f6f2ee1a288ca59eb8f94fdf77c6380
shuvenduofflinexoriant <77153463+shuvenduofflinexoriant@users.noreply.github.com>
2021-01-11 15:13:41 +0530

    added_database

--- src/com/xoriant/beans/BookType.java
+++ src/com/xoriant/beans/BookType.java
@@ -1,5 +1,13 @@
 package com.xoriant.beans;
 
 public enum BookType {
-		REFENCEBOOK, STORYBOOK, MAGAZINE
+		REFERENCEBOOK(7,5), STORYBOOK(15,3), MAGAZINE(15,4);
+		
+		private final int maxBorrowDays;
+		private final double finePerDay;
+		
+		BookType(int maxBorrowDays,double finePerDay){
+			this.maxBorrowDays = maxBorrowDays;
+			this.finePerDay = finePerDay;
+		}
 }
